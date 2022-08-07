f790fe6293282fdb3c334f76c2304983fad70d7e
Magnus Herweyer <magnus.herweyer@gmail.com>
2022-08-06 19:32:55 -0500

    Adds random number selection

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-from random import *
+import random
 #Number guesser is a game where a random number is selected and the player has to guess the right number in less than X guesses
+random_num_range_min = 1
+random_num_range_max = 200
 
 #starting number of guesses a user has
 number_of_guesses = 5
-random_num = 6
+random_num = random.randint(random_num_range_min, random_num_range_max)
+print(random_num)
 
-guess = int(input("What is your guess?"))
 while number_of_guesses > 0:
+    # turn = 1
+    # while turn == 1:
+    # guess = int(input("A random number between " + str(random_num_range_min) + " and " + str(random_num_range_max) + " has just been selected. What is your guess?"))
+
+    guess = int(input("What is your guess?"))
 
     if guess != random_num:
         number_of_guesses = number_of_guesses -1
         print(f'Remaining guesses:', number_of_guesses)
 
-    else: #guess == random_num:
-        print("Holy shit you aren't a nerd you WON")
+
+    else:
+        print("GAME WON: Holy shit you aren't a nerd you WON")
         number_of_guesses = -1
 
+#not happy with having to specific guesses = 0 but was getting issues with game winning guess printing win message and no more guesses message
 if number_of_guesses == 0:
     print("You ran out of guesses, game over NERD")
