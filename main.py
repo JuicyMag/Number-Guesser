0e9df2b7994b0c99abb5ccfa21197c971aed0dab
Magnus Herweyer <magnus.herweyer@gmail.com>
2022-08-06 22:16:04 -0500

    Makes fix to bug for when guess is during turn one

--- main.py
+++ main.py
@@ -13,23 +13,30 @@ random_num = random.randint(random_num_range_min, random_num_range_max)
 print(random_num)
 
 while number_of_guesses > 0:
-    if turn_number == 0:
-        guess = int(input("A random number between " + str(random_num_range_min) + " and " + str(random_num_range_max) + " has just been selected. What is your guess?"))
+    # if turn_number == 0:
+    #     guess = int(input("A random number between " + str(random_num_range_min) + " and " + str(random_num_range_max) + " has just been selected. What is your guess?"))
+    #     turn_number = turn_number + 1
+    #     number_of_guesses = number_of_guesses - 1
+    #     print(f'Remaining guesses:', number_of_guesses)
+    # else:
+    #     guess = int(input("Incorrect guess. What is your guess?"))
+    turn_number= 1
+    guess = int(input("What number would you like to guess?"))
+    if guess != random_num and turn_number == 1:
+        number_of_guesses = number_of_guesses -1
+        print(f'TESTESTETESTIncorrect guess. Remaining guesses:', number_of_guesses)
         turn_number = turn_number + 1
+
+    elif guess != random_num:
         number_of_guesses = number_of_guesses - 1
-        print(f'Remaining guesses:', number_of_guesses)
+        print(f'Incorrect guess. Remaining guesses:', number_of_guesses)
+
+
     else:
-        guess = int(input("Incorrect guess. What is your guess?"))
-
-        if guess != random_num:
-            number_of_guesses = number_of_guesses -1
-            print(f'Incorrect guess. Remaining guesses:', number_of_guesses)
-
-
-        else:
-            print("GAME WON: Holy shit you aren't a nerd you WON")
-            number_of_guesses = -1
+        print("GAME WON: Holy shit you aren't a nerd you WON")
+        number_of_guesses = -1
 
 #not happy with having to specific guesses = 0 but was getting issues with game winning guess printing win message and no more guesses message
 if number_of_guesses == 0:
     print("You ran out of guesses, game over NERD")
+
